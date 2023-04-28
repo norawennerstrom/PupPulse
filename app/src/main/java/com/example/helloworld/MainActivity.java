3e1177475c1c97913976237043a5b7593d45baa5
norawennerstrom <nora.wennerstrom@live.com>
2023-04-28 22:11:55 +0200

    modify activity_main.xml, MainActivity.java and strings.xml

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtLogIn = findViewById(R.id.logIn);
+        EditText edtTxtEmail = findViewById(R.id.enterEmail);
+        EditText edtTxtPassword = findViewById(R.id.enterPassword);
+        String userEmail = edtTxtEmail.getText().toString();
+
+        //TODO: Commit and push after writing code
+
     }
 }
