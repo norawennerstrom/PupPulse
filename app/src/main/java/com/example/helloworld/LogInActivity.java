7c0183c2ac9ab02956593ecc9be1e840ee3589ac
norawennerstrom <nora.wennerstrom@live.com>
2023-05-04 14:24:53 +0200

    added some files modified some files

--- app/src/main/java/com/example/helloworld/LogInActivity.java
+++ app/src/main/java/com/example/helloworld/LogInActivity.java
@@ -3,6 +3,8 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void onNextBtnClick (View view) { //TODO: Make it possible to log in to/create an account
+        EditText edtTxtEmail = findViewById(R.id.enterEmail);
+        EditText edtTxtPassword = findViewById(R.id.enterPassword);
+        String userEmail = edtTxtEmail.getText().toString();
+        String userPassword = edtTxtPassword.getText().toString();
+
     }
 }
