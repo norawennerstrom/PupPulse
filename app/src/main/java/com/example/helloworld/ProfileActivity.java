1ad065e8942a45879fe89c550240e12d7e856d04
sumeyayasir <124312355+sumeyayasir@users.noreply.github.com>
2023-05-08 20:30:41 +0200

    modify AndroidManifest.xml

--- app/src/main/java/com/example/helloworld/ProfileActivity.java
+++ app/src/main/java/com/example/helloworld/ProfileActivity.java
@@ -3,12 +3,27 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        btn1 =(Button)findViewById(R.id.Backbutton);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void OnClick(View v){
+                Intent intent=new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+
+        });
     }
+
+
 }
