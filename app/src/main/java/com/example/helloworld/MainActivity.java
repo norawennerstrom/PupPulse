7841bf0001528dc1d01d278631b71d29af02aaee
norawennerstrom <nora.wennerstrom@live.com>
2023-05-11 14:58:29 +0200

    modified build.gradle, fragment_statistics.xml, MainActivity.java, settings.gradle, statisticsFragment.java

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -13,7 +13,6 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //ActivityMainBinding binding;
     BottomNavigationView bottomNavigationView;
     HomeFragment home = new HomeFragment();
     notificationsFragment notifications = new notificationsFragment();
