87b74062559b9a3d5ee00047b20e677f103b6190
norawennerstrom <nora.wennerstrom@live.com>
2023-05-11 12:15:29 +0200

    modified MainActivity.java

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.helloworld;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,11 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -55,36 +50,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //binding = ActivityMainBinding.inflate(getLayoutInflater());
-        //setContentView(binding.getRoot());
-        //replaceFragment(new HomeFragment());
-
-        //binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-
-            //switch (item.getItemId()) {
-           //     case R.id.home_menu_item:
-           //         replaceFragment(new HomeFragment());
-           //         break;
-           //     case R.id.profile_menu_item:
-           //         replaceFragment(new profileFragment());
-           //         break;
-           //     case R.id.statistics_menu_item:
-           //         replaceFragment(new statisticsFragment());
-           //         break;
-           //     case R.id.notifications_menu_item:
-           //         replaceFragment(new notificationsFragment());
-           //         break;
-            //}
-
-        //    return true;
-        //});
-
     }
 
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
-    }
 }
