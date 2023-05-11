9377249445b11d4f57a8cbbb5740b1a3a6e50681
norawennerstrom <nora.wennerstrom@live.com>
2023-05-11 15:23:20 +0200

    modified HomeFragment.java and profileFragment.java

--- app/src/main/java/com/example/helloworld/profileFragment.java
+++ app/src/main/java/com/example/helloworld/profileFragment.java
@@ -14,6 +14,8 @@ public class profileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        // Write code here!
+        return view;
     }
 }
