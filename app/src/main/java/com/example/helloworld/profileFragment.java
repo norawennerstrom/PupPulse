6b53a5736ebabd7fd08587d20a09ffd57b419a73
norawennerstrom <nora.wennerstrom@live.com>
2023-05-13 15:44:31 +0200

    modified fragment_profile.xml and profileFragment.java

--- app/src/main/java/com/example/helloworld/profileFragment.java
+++ app/src/main/java/com/example/helloworld/profileFragment.java
@@ -1,21 +1,49 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class profileFragment extends Fragment {
+
+    Button btn1;
+    Button btn2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        // Write code here!
+        //setContentView(R.layout.activity_profile);
+        btn1 =(Button)view.findViewById(R.id.Backbutton);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                HomeFragment home = new HomeFragment();
+                getParentFragmentManager().beginTransaction().replace(R.id.frame_layout, home).commit();
+            }
+
+        });
+
+        btn2=view.findViewById(R.id.skapakontobuttom);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(), Create_account.class);
+                startActivity(intent);
+
+            }
+
+        });
+
         return view;
     }
 }
