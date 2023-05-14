9d18a36536002c331be6800fd448d7fa22b1f6d0
norawennerstrom <nora.wennerstrom@live.com>
2023-05-14 14:55:22 +0200

    modified HomeFragment.java

--- app/src/main/java/com/example/helloworld/HomeFragment.java
+++ app/src/main/java/com/example/helloworld/HomeFragment.java
@@ -7,14 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
+
+    TextView currentPulse;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        // Write code here!
+        currentPulse = view.findViewById(R.id.currentPulse);
+        currentPulse.setText("95 bpm");
         return view;
     }
 }
