4b5606ce8ce34a6e6cac9c9440988ad665eda211
norawennerstrom <nora.wennerstrom@live.com>
2023-05-14 15:24:58 +0200

    modified HomeFragment.java and colors.xml

--- app/src/main/java/com/example/helloworld/HomeFragment.java
+++ app/src/main/java/com/example/helloworld/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,14 +12,25 @@ import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
 
-    TextView currentPulse;
+    TextView currentPulseTextView;
+    int currentPulse;
+    int highPulse = 120; // Depends on dog's breed and age!
+    int lowPulse = 70;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        currentPulse = view.findViewById(R.id.currentPulse);
-        currentPulse.setText("95 bpm");
+        currentPulseTextView = view.findViewById(R.id.currentPulse);
+        currentPulse = 95; // TODO: create a function that would be used to retrieve the actual pulse from the heart rate monitor
+        currentPulseTextView.setText(String.valueOf(currentPulse));
+        if(currentPulse > highPulse) {
+            currentPulseTextView.setTextColor(ContextCompat.getColor(getContext(), R.color.red));
+        } else if(currentPulse > lowPulse) {
+            currentPulseTextView.setTextColor(ContextCompat.getColor(getContext(), R.color.green));
+        } else if(currentPulse < lowPulse) {
+            currentPulseTextView.setTextColor(ContextCompat.getColor(getContext(), R.color.blue));
+        }
         return view;
     }
 }
