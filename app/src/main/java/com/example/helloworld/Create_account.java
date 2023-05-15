b438a4c171dcc1a3390671fa3e1e93f8bfbb6a9f
sumeyayasir <124312355+sumeyayasir@users.noreply.github.com>
2023-05-15 11:43:25 +0200

    modify activity_create_account2.xml

--- app/src/main/java/com/example/helloworld/Create_account.java
+++ app/src/main/java/com/example/helloworld/Create_account.java
@@ -1,16 +1,31 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Create_account extends AppCompatActivity {
+    Button btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account2);
 
-    }
+        btn4 =(Button)findViewById(R.id.button2);
+            btn4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    Intent intent=new Intent(Create_account.this, Profil_with_account.class);
+                    startActivity(intent);
+                }
+
+            });
+
+        }
 }
 
 
