30c8dcb84627e788118c3a78d6a29ac6e3a445ea
William <willewass@hotmail.com>
2023-05-21 11:37:45 +0200

    Ändringar i settings

--- app/src/main/java/com/example/helloworld/SettingsActivity.java
+++ app/src/main/java/com/example/helloworld/SettingsActivity.java
@@ -3,6 +3,9 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+
+    }
+
+    public void onBtnClick (View view) {
+
 
     }
 }
